708c17ff6bdb09838df4e9912258ecb0c83d81d1
Yongjin Lee <yl2889@cornell.edu>
2025-03-13 15:03:52 +0900

    bunch of changes

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const _ = require('lodash');
+const { log } = require('console');
+const {readFile} = require('fs');
+const getText = (path) => {
+    return new Promise((resolve,reject)=>{
+        readFile(path, 'utf8', (err,data)=> {
+            if(err){
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+// getText('./content/first.txt').then(result => console.log(result)).catch((err) => console.log(err))
 
-const items = [1,[2,[3,[4]]]];
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+const start = async() => {
+    const first = await getText('./content/first.txt')
+    console.log(first)
+}
+
+start()
